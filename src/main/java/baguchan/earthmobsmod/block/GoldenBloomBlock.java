5a669734f943ceb1f77a0a2df100c0e88f9a25cf
pentantan <shutux500@gmail.com>
2019-08-04 07:59:24 +0900

    0.0.2.7 when you collision goldenbloom,give slow regeneration

---
+++ src/main/java/baguchan/earthmobsmod/block/GoldenBloomBlock.java
@@ -0,0 +1,52 @@
+package baguchan.earthmobsmod.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FlowerBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.Random;
+
+public class GoldenBloomBlock extends FlowerBlock {
+	public GoldenBloomBlock(Effect effectIn, int duration, Block.Properties propertiesIn) {
+		super(effectIn, duration, propertiesIn);
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+		VoxelShape voxelshape = this.getShape(stateIn, worldIn, pos, ISelectionContext.dummy());
+		Vec3d vec3d = voxelshape.getBoundingBox().getCenter();
+		double d0 = (double) pos.getX() + vec3d.x;
+		double d1 = (double) pos.getZ() + vec3d.z;
+
+		/*for(int i = 0; i < 3; ++i) {
+			if (rand.nextBoolean()) {
+				worldIn.addParticle(ParticleTypes.SMOKE, d0 + (double)(rand.nextFloat() / 5.0F), (double)pos.getY() + (0.5D - (double)rand.nextFloat()), d1 + (double)(rand.nextFloat() / 5.0F), 0.0D, 0.0D, 0.0D);
+			}
+		}*/
+
+	}
+
+	public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
+		if (!worldIn.isRemote && worldIn.rand.nextInt(220) == 0) {
+			if (entityIn instanceof LivingEntity) {
+				LivingEntity livingentity = (LivingEntity) entityIn;
+
+				livingentity.addPotionEffect(new EffectInstance(Effects.REGENERATION, 60));
+
+			}
+
+		}
+	}
+}
--- src/main/java/baguchan/earthmobsmod/handler/EarthBlocks.java
+++ src/main/java/baguchan/earthmobsmod/handler/EarthBlocks.java
@@ -1,9 +1,11 @@
 package baguchan.earthmobsmod.handler;
 
 import baguchan.earthmobsmod.block.EarthFluidBlock;
+import baguchan.earthmobsmod.block.GoldenBloomBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FlowerBlock;
+import net.minecraft.block.FlowerPotBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
@@ -15,13 +17,15 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class EarthBlocks {
 
     public static final Block MUDWATER = new EarthFluidBlock(EarthFluids.MUD_WATER, Block.Properties.create(Material.WATER).doesNotBlockMovement().hardnessAndResistance(100.0F).lootFrom(Blocks.AIR));
-    public static final FlowerBlock GOLDENBLOOM = new FlowerBlock(Effects.REGENERATION, 160, Block.Properties.create(Material.PLANTS).doesNotBlockMovement().hardnessAndResistance(0.0F).sound(SoundType.PLANT));
+    public static final FlowerBlock GOLDENBLOOM = new GoldenBloomBlock(Effects.REGENERATION, 160, Block.Properties.create(Material.PLANTS).doesNotBlockMovement().hardnessAndResistance(0.0F).sound(SoundType.PLANT));
+    public static final FlowerPotBlock POTTED_GOLDENBLOOM = new FlowerPotBlock(GOLDENBLOOM, Block.Properties.create(Material.PLANTS).doesNotBlockMovement().hardnessAndResistance(0.0F).sound(SoundType.PLANT));
 
 
     public static void registerBlocks(IForgeRegistry<Block> registry) {
         //Terrain
         registry.register(MUDWATER.setRegistryName("mud"));
         registry.register(GOLDENBLOOM.setRegistryName("golden_bloom"));
+        registry.register(POTTED_GOLDENBLOOM.setRegistryName("potted_golden_bloom"));
     }
 
     public static void registerItemBlocks(IForgeRegistry<Item> registry) {
