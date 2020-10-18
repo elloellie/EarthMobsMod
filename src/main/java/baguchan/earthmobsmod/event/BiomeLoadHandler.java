f2abd761234553fd112d866bdfff69753de92925
shutux <shutux500@gmail.com>
2020-10-18 11:47:07 +0900

    add bone spider spawn(using BiomeDictionary)

--- src/main/java/baguchan/earthmobsmod/event/BiomeLoadHandler.java
+++ src/main/java/baguchan/earthmobsmod/event/BiomeLoadHandler.java
@@ -4,6 +4,7 @@ import baguchan.earthmobsmod.EarthMobsMod;
 import baguchan.earthmobsmod.handler.EarthBlocks;
 import baguchan.earthmobsmod.handler.EarthEntitys;
 import net.minecraft.entity.EntityClassification;
+import net.minecraft.util.RegistryKey;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.world.biome.Biome;
@@ -15,6 +16,7 @@ import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.IFeatureConfig;
 import net.minecraft.world.gen.placement.ChanceConfig;
 import net.minecraft.world.gen.placement.Placement;
+import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -34,6 +36,8 @@ public class BiomeLoadHandler {
     public static void loadingBiome(BiomeLoadingEvent event) {
         BiomeGenerationSettingsBuilder generation = event.getGeneration();
 
+        RegistryKey<Biome> biome = RegistryKey.getOrCreateKey(Registry.BIOME_KEY, event.getName());
+
         if (event.getName().toString().contains("minecraft:flower_forest")) {
             event.getSpawns().getSpawner(EntityClassification.CREATURE).add(new MobSpawnInfo.Spawners(EarthEntitys.MOOBLOOM, 10, 2, 3));
         }
@@ -42,13 +46,17 @@ public class BiomeLoadHandler {
             event.getSpawns().getSpawner(EntityClassification.CREATURE).add(new MobSpawnInfo.Spawners(EarthEntitys.CLUCKSHROOM, 8, 2, 3));
         }
 
-        if (event.getName().getNamespace().toString().contains("minecraft") || event.getName().getNamespace().toString().contains("biomesoplenty")) {
-            if (event.getCategory() == Biome.Category.SWAMP) {
+        if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.OVERWORLD)) {
+            if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.SWAMP)) {
                 event.getGeneration().withFeature(GenerationStage.Decoration.LAKES, LAKE_MUD);
             }
 
-            if (event.getCategory() == Biome.Category.EXTREME_HILLS) {
+            if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.MOUNTAIN)) {
                 event.getSpawns().getSpawner(EntityClassification.CREATURE).add(new MobSpawnInfo.Spawners(EarthEntitys.HORNED_SHEEP, 6, 2, 3));
+            }
+
+            if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.SPOOKY)) {
+                event.getSpawns().getSpawner(EntityClassification.MONSTER).add(new MobSpawnInfo.Spawners(EarthEntitys.BONE_SPIDER, 40, 2, 3));
             }
         }
     }
